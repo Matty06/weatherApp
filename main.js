8a321abc260696487ba6b6f6476b364d183ee459
Matěj Kunc <matej.kunc@rondogo.com>
2020-10-19 21:02:33 +0200

    Temperature K -->     let tempC = data.main.temp - 273.15;

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-const api_url = "http://api.openweathermap.org/data/2.5/weather?q=Praha&appid=0333dd85d5639a156592d764193fbab5";
+const apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Praha&appid=0333dd85d5639a156592d764193fbab5";
 const temperature = document.getElementById("temperature");
 const place = document.getElementById("place");
 
 async function getTemp() {
-    const response = await fetch(api_url);
+    const response = await fetch(apiUrl);
     const data = await response.json();
+    let tempC = data.main.temp - 273.15;
+
     place.innerHTML = data.name;
-    temperature.innerHTML = data.main.temp;
+    temperature.innerHTML = Math.round(tempC) + " °C";
 }
+
 getTemp();
+
+
+
