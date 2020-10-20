7806754a12446de827aa2b4079a9d382ba472666
Matěj Kunc <matej.kunc@rondogo.com>
2020-10-20 22:07:15 +0200

    Added: btnSearch city

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-let city = "Praha";
-const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0333dd85d5639a156592d764193fbab5`;
-
-const temperature = document.getElementById("temperature");
 const place = document.getElementById("place");
+let city = place.value;
+const temperature = document.getElementById("temperature");
 const time = document.getElementById("time");
 const windspeed = document.getElementById("windspeed");
+const btnSearch = document.getElementById("search");
+
 
 window.onload = () => {
     setInterval(getTime, 1000);
+    getTemp(city);
 }
 
-function getTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    time.innerHTML = h + ":" + m + ":" + s;
-  }
+btnSearch.onclick = () => {
+    // update city
+    city = place.value;
+    console.log(city);
+    getTemp(city);
+}
 
-
-async function getTemp() {
-    const response = await fetch(apiUrl);
+async function getTemp(city) {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0333dd85d5639a156592d764193fbab5`);
     const data = await response.json();
     let tempC = data.main.temp - 273.15;
     console.log(data);
@@ -30,7 +29,12 @@ async function getTemp() {
     temperature.innerHTML = Math.round(tempC) + " °C";
 }
 
-getTemp();
-
+function getTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    time.innerHTML = h + ":" + m + ":" + s;
+  }
 
 
